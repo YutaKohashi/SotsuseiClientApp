d390106d53bd08da083486c73fed19f2605d4615
YutaKohashi <kaerukerokero1995@gmail.com>
2017-10-11 15:31:55 +0900

    戻り値追加

--- app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/service/ServiceObjectExtension.kt
+++ app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/service/ServiceObjectExtension.kt
@@ -3,7 +3,6 @@ package jp.yuta.kohashi.sotsuseiclientapp.service
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
 import jp.yuta.kohashi.sotsuseiclientapp.App
 
 /**
@@ -12,35 +11,27 @@ import jp.yuta.kohashi.sotsuseiclientapp.App
  * Date : 10 / 10 / 2017
  */
 
-interface ServiceObjectExtension<T: Any> {
-
-    companion object {
-        val ALREADY_RUNNING:String = "already_running"
-        val SUCCESS_RUN:String = "success_run"
-        val ALREADY_STOPPED = "already_stopped"
-        val SUCCESS_STOP = "success_stop"
-    }
+interface ServiceObjectExtension<T : Any> {
 
     /**
      * サービスが起動しているか
      */
-    fun isRunning():Boolean{
+    fun isRunning(): Boolean {
         val am = App.context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         val listServiceInfo = am.getRunningServices(Integer.MAX_VALUE)
         return listServiceInfo.any { it.service.className == ((this as Object).`class` as Class<T>).name }
-
     }
 
     /**
      * サービス起動
      * 戻り値は起動に成功かすでに起動しているか
      */
-    fun start():String{
-        return if(isRunning()){
+    fun start(): ServiceStateResult {
+        return if (isRunning()) {
             App.context.startService(Intent(App.context, ((this as Object).`class` as Class<T>).javaClass))
-            SUCCESS_RUN
+            ServiceStateResult.SUCCESS_RUN
         } else {
-            ALREADY_RUNNING
+            ServiceStateResult.ALREADY_RUNNING
         }
     }
 
@@ -48,12 +39,19 @@ interface ServiceObjectExtension<T: Any> {
      * サービス停止
      * 戻り値はすでにに停止しているかどうか
      */
-    fun stop():String{
-        return if(isRunning()){
+    fun stop(): ServiceStateResult {
+        return if (isRunning()) {
             App.context.stopService(Intent(App.context, ((this as Object).`class` as Class<T>).javaClass))
-            SUCCESS_STOP
+            ServiceStateResult.SUCCESS_STOP
         } else {
-            ALREADY_STOPPED
+            ServiceStateResult.ALREADY_STOPPED
         }
     }
+}
+
+enum class ServiceStateResult {
+    ALREADY_RUNNING,
+    SUCCESS_RUN,
+    ALREADY_STOPPED,
+    SUCCESS_STOP
 }
