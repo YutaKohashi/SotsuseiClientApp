8415dda490aa6915f95472dde79b07e618db42ea
YutaKohashi <kaerukerokero1995@gmail.com>
2017-10-11 15:02:37 +0900

    サービスのオブジェクトエクステンションを追加

--- app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/service/ServiceObjectExtension.kt
+++ app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/service/ServiceObjectExtension.kt
@@ -1,5 +1,11 @@
 package jp.yuta.kohashi.sotsuseiclientapp.service
 
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import jp.yuta.kohashi.sotsuseiclientapp.App
+
 /**
  * Author : yutakohashi
  * Project name : SotsuseiClientApp
@@ -15,15 +21,39 @@ interface ServiceObjectExtension<T: Any> {
         val SUCCESS_STOP = "success_stop"
     }
 
-    fun isRunning(){
+    /**
+     * サービスが起動しているか
+     */
+    fun isRunning():Boolean{
+        val am = App.context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val listServiceInfo = am.getRunningServices(Integer.MAX_VALUE)
+        return listServiceInfo.any { it.service.className == ((this as Object).`class` as Class<T>).name }
 
     }
 
-    fun start(){
-
+    /**
+     * サービス起動
+     * 戻り値は起動に成功かすでに起動しているか
+     */
+    fun start():String{
+        return if(isRunning()){
+            App.context.startService(Intent(App.context, ((this as Object).`class` as Class<T>).javaClass))
+            SUCCESS_RUN
+        } else {
+            ALREADY_RUNNING
+        }
     }
 
-    fun stop(){
-
+    /**
+     * サービス停止
+     * 戻り値はすでにに停止しているかどうか
+     */
+    fun stop():String{
+        return if(isRunning()){
+            App.context.stopService(Intent(App.context, ((this as Object).`class` as Class<T>).javaClass))
+            SUCCESS_STOP
+        } else {
+            ALREADY_STOPPED
+        }
     }
 }
