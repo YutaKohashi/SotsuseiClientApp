87d87f6af7163312eb5d83155d0c376e7e767ed1
YutaKohashi <kaerukerokero1995@gmail.com>
2017-10-11 15:32:26 +0900

    toast処理をまとめる

---
+++ app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/ui/ToastHelper.kt
@@ -0,0 +1,32 @@
+package jp.yuta.kohashi.sotsuseiclientapp.ui
+
+import android.widget.Toast
+import jp.yuta.kohashi.sotsuseiclientapp.App
+import jp.yuta.kohashi.sotsuseiclientapp.R
+import jp.yuta.kohashi.sotsuseiclientapp.utils.ResUtil
+
+/**
+ * Author : yutakohashi
+ * Project name : SotsuseiClientApp
+ * Date : 11 / 10 / 2017
+ */
+
+object ToastHelper {
+
+    fun alreadyRunningService() = show(ResUtil.getString(R.string.already_running_servce))
+
+    fun runService() = show(ResUtil.getString(R.string.success_runnning_service))
+
+    fun stopService() = show(ResUtil.getString(R.string.success_stop_servivce))
+
+    fun alreadyStopService() = show(ResUtil.getString(R.string.already_stop_service))
+
+    fun show(text: String) {
+        Toast.makeText(App.context, text, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showLong(text: String) {
+        Toast.makeText(App.context, text, Toast.LENGTH_LONG).show()
+    }
+
+}
