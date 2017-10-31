3ba770c822b6d47f8fa7977276c5f78f2364a3e8
YutaKohashi <kaerukerokero1995@gmail.com>
2017-11-01 00:00:15 +0900

    activity削除遷移処理変更

--- app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/ui/running/RunningFragment.kt
+++ app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/ui/running/RunningFragment.kt
@@ -1,10 +1,19 @@
 package jp.yuta.kohashi.sotsuseiclientapp.ui.running
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.transition.TransitionInflater
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import jp.yuta.kohashi.sotsuseiclientapp.R
 import jp.yuta.kohashi.sotsuseiclientapp.service.SotsuseiClientAppService
 import jp.yuta.kohashi.sotsuseiclientapp.service.StateResult
 import jp.yuta.kohashi.sotsuseiclientapp.ui.BaseFragment
 import jp.yuta.kohashi.sotsuseiclientapp.ui.ToastHelper
+import jp.yuta.kohashi.sotsuseiclientapp.ui.home.HomeActivity
 import kotlinx.android.synthetic.main.fragment_running.*
 
 /**
@@ -13,34 +22,40 @@ import kotlinx.android.synthetic.main.fragment_running.*
  * Date : 29 / 09 / 2017
  */
 
-class RunningFragment: BaseFragment(){
+class RunningFragment : BaseFragment() {
 
     override val sLayoutRes: Int
         get() = R.layout.fragment_running
 
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        when (SotsuseiClientAppService.start()) {
+            StateResult.ALREADY_RUNNING -> ToastHelper.alreadyRunningService()
+            StateResult.SUCCESS_RUN -> ToastHelper.runService()
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+    }
+
     override fun setEvent() {
 
-        /**
-         * シャッタボタン
-         */
-        shutterButton.setOnClickListener {
-
-        }
-
-        /**
-         * 違法駐車ボタン
-         */
-        ihoChushaButton.setOnClickListener {
-
-        }
         /**
          * ストップボタン
          */
         stopButton.setOnClickListener {
-            when(SotsuseiClientAppService.stop(SotsuseiClientAppService::class.java)){
+            when (SotsuseiClientAppService.stop(SotsuseiClientAppService::class.java)) {
                 StateResult.SUCCESS_STOP -> ToastHelper.stopService()
                 StateResult.ALREADY_STOPPED -> ToastHelper.alreadyStopService()
             }
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                if (activity.supportFragmentManager.backStackEntryCount != 0) activity.supportFragmentManager.popBackStack()
+            }, 400)
+
+
         }
 
 
