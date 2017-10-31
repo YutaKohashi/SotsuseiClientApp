2f4f7e57d5e13cfe2e5d2915b23fa3df57f1dc46
YutaKohashi <kaerukerokero1995@gmail.com>
2017-10-31 23:59:42 +0900

    汎用メソッド追加

--- app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/utils/Util.kt
+++ app/src/main/java/jp/yuta/kohashi/sotsuseiclientapp/utils/Util.kt
@@ -1,20 +1,30 @@
 package jp.yuta.kohashi.sotsuseiclientapp.utils
 
+import android.content.Context
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
 /**
  * Author : yutakohashi
  * Project name : SotsuseiClientApp
  * Date : 10 / 10 / 2017
  */
 
-object Util{
+object Util {
 
 
     /**
      * ブロードキャストレシーバが登録されているか
      * 引数:Intentクラスの定数を使用
      */
-    fun registeredBroadcastReceiver(receiver:String):Boolean{
+    fun registeredBroadcastReceiver(receiver: String): Boolean {
         return false
+    }
+
+    fun layoutRes2View(context: Context, @LayoutRes layoutRes: Int, root: ViewGroup, attachToRoot: Boolean = false): View {
+        return LayoutInflater.from(context).inflate(layoutRes, root, attachToRoot)
     }
 
 
